a654b94b7b0768c876bd3967fe32f3496757637a
Peter Farrow <pnfarrow@gmail.com>
2024-07-24 12:14:38 -0600

    true initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import "./App.css";
-import { Character } from "./model";
 
 function App() {
-  async function getCharacters() {
-    const characters = await Character.findAll();
-
-    const charList = characters.map((char) => {
-      <li>{char.firstName}</li>;
-    });
-
-    return charList;
-  }
-
   return (
     <>
       <h1>Here we go!</h1>
-      <ul>{getCharacters()}</ul>
     </>
   );
 }
